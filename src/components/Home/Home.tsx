85fd29379ac5d9582d06a45d353ebff76151612b
cannh <cannhhe130043@gmail.com>
2020-09-03 17:11:10 +0700

    add home component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 50%;
+  height: 100%;
+  margin: auto;
+  background-color: red;
+`
+
+export const Home: React.FC = () => {
+  return <Container></Container>
+}
