a8b8e1eab487148907983e422b6b956f0dbf928b
cannh <cannhhe130043@gmail.com>
2020-09-05 02:33:27 +0700

    add navbar

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import styled from 'styled-components'
 import { useAuth } from '../../contexts/AuthContext'
 import { Login } from '../Auth'
 import { Home } from '../Home'
+import { Navbar } from '../Nav'
+import styled from 'styled-components'
+const Container = styled.div`
+  width: 95%;
+  margin: auto;
+  position: relative;
+`
 
 export const Main: React.FC = () => {
   const { token } = useAuth()
-  return token ? <Home /> : <Login />
+  return token ? (
+    <Container>
+      <Navbar />
+      <Home />
+    </Container>
+  ) : (
+    <Login />
+  )
 }
---
+++ src/components/Nav/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.nav`
+  height: 10%;
+  
+`
+
+export const Navbar: React.FC = () => {
+  return <Container></Container>
+}
