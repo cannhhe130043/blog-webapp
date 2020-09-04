c47bc76b30e0191b86fdba6c8d60efde020fb695
cannh <cannhhe130043@gmail.com>
2020-09-04 14:15:03 +0700

    add background image

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import React from 'react'
 import './App.css'
 import { AuthProvider } from './contexts/AuthContext'
 import { Main } from './components/Main'
+import styled from 'styled-components'
+
+const Container = styled.div<{
+  style: {
+    url: string
+  }
+}>`
+  text-align: center;
+  height: 100vh;
+  width: 100vw;
+  background-size: 100% 100%;
+  background-image: url(${(props) => props.style.url});
+`
 
 function App() {
   return (
-    <div className="App">
+    <Container style={{ url: `${window.location.origin}/images/bg.jpg` }}>
       <AuthProvider>
         <Main />
       </AuthProvider>
-    </div>
+    </Container>
   )
 }
 
