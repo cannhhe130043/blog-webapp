b94221b263218d7fc4b72f77211d30404571fedb
= <=>
2020-09-08 13:54:12 +0700

    modify home

---
+++ src/routes/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 50vw;
+  height: 100vh;
+  margin: auto;
+  background-color: red;
+`
+
+export const Home: React.FC = () => {
+  return <Container></Container>
+}
