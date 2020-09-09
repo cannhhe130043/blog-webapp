a2e3726bf13aaff98d8f5907ef95663d3a4c0eb9
= <=>
2020-09-09 15:17:09 +0700

    test logout button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useAuth } from '../contexts/AuthContext'
+import { useHistory } from 'react-router-dom'
 
 const Container = styled.nav`
-  height: 10%;
+  height: 5%;
+  width: 100%;
+  position: fixed;
+  background-color: red;
 `
 
 export const Navbar: React.FC = () => {
-  return <Container></Container>
+  const history = useHistory()
+
+  const handleLogout = () => {
+    history.push('/login')
+    window.location.reload()
+  }
+  return (
+    <Container>
+      <button onClick={handleLogout}>logout</button>
+    </Container>
+  )
 }
